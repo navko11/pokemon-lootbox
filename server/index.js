8ec33be85f87dd862bd6fb090c1647d5c1a35ef1
mark <marklca@hotmail.com>
2024-08-10 19:01:28 +1000

    initialize server side with express and mongoose

---
+++ server/index.js
@@ -0,0 +1,11 @@
+
+
+const express = require("express")
+const mongoose = require("mongoose")
+const cors = require("cors")
+
+const app =express()
+app.use(express.json())
+app.use(cors())
+
+mongoose.connect()
