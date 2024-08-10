8a5db245418206457e05ba9808c4bb47551f8ba2
mark <marklca@hotmail.com>
2024-08-10 21:40:48 +1000

    inventory, lootbox page, shop page routes added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,9 @@ import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import RegistrationForm from './registeruser'
 import Login from './userlogin'
 import Home from './home'
+import Inventory from './inventory'
+import Lootboxpage from './lootboxpage'
+import ShopPage from './shop'
 
 function App() {
 
@@ -13,6 +16,9 @@ function App() {
         <Route path='/register' element={<RegistrationForm/>} />
         <Route path='/login' element={<Login />} />
         <Route path='/home' element={<Home />} />
+        <Route path='/inventory' element={<Inventory />} />
+        <Route path='/lootbox' element={<Lootboxpage />} />
+        <Route path='/shop' element={<ShopPage />} />
       </Routes>
     </BrowserRouter>
   )
--- client/src/shop.jsx
+++ client/src/shop.jsx
@@ -3,19 +3,13 @@ import Header from './components/header'
 import Footer from './components/footer'
 import { Link } from 'react-router-dom'
 
-function Home() {
+function ShopPage() {
     return (
       <div>
         <Header />
-        <div>
-        <h1>Welcome</h1>
-        <Link to="/inventory">View Inventory</Link>
-        <br></br>
-        <Link to="/lootbox">Open Lootbox</Link>
         <Footer />
-        </div>
       </div>
     );
   }
 
-export default Home;
+export default ShopPage;
