4c332b7a403043660ba23d19281cf367a89183a4
mark <marklca@hotmail.com>
2024-08-12 20:28:33 +1000

    return button on aboutpage

--- client/src/about.jsx
+++ client/src/about.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from './components/header'
 import Footer from './components/footer'
 import './styles/about.css'
+import { Link } from 'react-router-dom';
 
 function About() {
   return (
@@ -14,6 +15,7 @@ function About() {
         <p>In this development phase you will have an infinite amount of credits supplied - so you can pull as many lootboxes as you want! Note: The inventory option on the main menu is not yet functional so you will not be able to view all the pokemon you have obtained from the lootbox.</p>
         <p>The Pokemon obtainable in this version 0.1.0 is a randomized selection of 50 of the 151 first generation Pokemon. The chances for pulling Pokemon by tier are COMMON = 80%  | <span className="highlight-blue">RARE = 19%</span> | and <span className="highlight-gold">LEGENDARY = 1%</span></p>
         <p>Goodluck and go catch 'em all!</p>
+        <Link to="/home"><button type="button" className="return">Return</button></Link>
       <Footer />
     </div>
   );
